6c80d6d47ed65c360bc8640bd80ae61c592a9f5b
Sebastián Márquez <sebamarquezblanco@gmail.com>
2022-08-29 20:27:39 -0300

    2da modificacion

--- grupal2/listado/js/almacenar.js
+++ grupal2/listado/js/almacenar.js
@@ -1,26 +1,43 @@
-let guardar = document.getElementById("contenedor")
-let id = 0
+function mostrarListaGuardada() {
+    let array = localStorage
+    for (let i = 0; i < array.length; i++) {
+        guardar.innerHTML += `
+        <li>`+ array[i] + `</li>
+`   
+    id = localStorage.length
+    }
+}
 
-console.log(document.getElementById("item").value)
+let id = 0
+let guardar = document.getElementById("contenedor")
+
 document.getElementById("agregar").addEventListener("click",function(){
     lists = document.getElementById("item").value
     agregar_html = `` 
-    if(lists.length>0){
+        if(lists.length>0){
+            localStorage.setItem(id,lists)
+            valor = localStorage.getItem(id)
+            agregar_html+=`
+            <li>${valor}</li>
+            `
+            id += 1
+            guardar.innerHTML += agregar_html
+            document.getElementById("item").value = ""
+        }
+        });
         
-        localStorage.setItem(id,lists)
-        valor = localStorage.getItem(id)
-        agregar_html+=`
-        <li>`+valor+`</li>
-        `
-        id += 1
-    guardar.innerHTML += agregar_html
-    valor = ""
-    }
+    
+        document.getElementById("limpiar").addEventListener("click",function(){
+            localStorage.clear()
+            id = 0
+            guardar.innerHTML = ``
+        })
+document.addEventListener("DOMContentLoaded", mostrarListaGuardada);
 
-});
 
-document.getElementById("limpiar").addEventListener("click",function(){
-    localStorage.clear()
-    id = 0
-    guardar.innerHTML = ``
-})
+
+        
+        
+    
+
+
